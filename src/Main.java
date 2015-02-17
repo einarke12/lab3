12a09003e45f02e80cad264c76d7008c6b15e15c
davidk12 <davidk12@ru.is>
2015-02-17 21:29:44 +0000

    stuff

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
-import java.util.List;
+import aima.core.search.csp.*;
 
-import aima.core.search.csp.Assignment;
-import aima.core.search.csp.CSP;
-import aima.core.search.csp.CSPStateListener;
-import aima.core.search.csp.Domain;
-import aima.core.search.csp.ImprovedBacktrackingStrategy;
-import aima.core.search.csp.NotEqualConstraint;
-import aima.core.search.csp.SolutionStrategy;
-import aima.core.search.csp.Variable;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
@@ -32,35 +26,123 @@ public class Main {
 //		# The Norwegian lives next to the blue house.
 //
 //		Now, who drinks water? Who owns the zebra?
-				
-		String[] colors = {"Red", "Green", "Ivory", "Yellow", "Blue"};
+
+
+		/*String[] colors = {"Red", "Green", "Ivory", "Yellow", "Blue"};
 		String[] nations = {"Englishman", "Spaniard", "Norwegian", "Ukrainian", "Japanese"};
 		String[] cigarettes = {"Old Gold", "Kools", "Chesterfields", "Lucky Strike", "Parliaments"};
 		String[] drink = {"Water", "Orange juice", "Tea", "Coffee", "Milk"};
-		String[] pet = {"Zebra", "Dog", "Fox", "Snails", "Horse"};
-		
+		String[] pet = {"Zebra", "Dog", "Fox", "Snails", "Horse"};*/
+
+		String[] houseNumbers = {"1", "2", "3", "4", "5"};
+
+		List<Variable> variables = new LinkedList<Variable>();
+
 		// TODO create variables, e.g.,
-		// Variable var1 = new Variable("name of the variable 1");
-		// Variable var2 = new Variable("name of the variable 2");
-		
-		List<Variable> variables = null;
+		Variable Red = new Variable("Red");
+		Variable Green = new Variable("Green");
+		Variable Ivory = new Variable("Ivory");
+		Variable Yellow = new Variable("Yellow");
+		Variable Blue = new Variable("Blue");
+
+		Variable Englishman = new Variable("Englishman");
+		Variable Spaniard = new Variable("Spaniard");
+		Variable Norwegian = new Variable("Norwegian");
+		Variable Ukrainian = new Variable("Ukrainian");
+		Variable Japanese = new Variable("Japanese");
+
+		Variable OldGold = new Variable("OldGold");
+		Variable Kools = new Variable("Kools");
+		Variable Chesterfields = new Variable("Chesterfields");
+		Variable LuckyStrike = new Variable("LuckyStrike");
+		Variable Parliaments = new Variable("Parliaments");
+
+		Variable Water = new Variable("Water");
+		Variable OrangeJuice = new Variable("OrangeJuice");
+		Variable Tea = new Variable("Tea");
+		Variable Coffee = new Variable("Coffee");
+		Variable Milk = new Variable("Milk");
+
+		Variable Zebra = new Variable("Zebra");
+		Variable Dog = new Variable("Dog");
+		Variable Fox = new Variable("Fox");
+		Variable Snails = new Variable("Snails");
+		Variable Horse = new Variable("Horse");
+
 		// TODO add all your variables to this list, e.g.,
-		// variables.add(var1);
-		// variables.add(var2);
+		variables.add(Red);
+		variables.add(Green);
+		variables.add(Ivory);
+		variables.add(Yellow);
+		variables.add(Blue);
+
+		variables.add(Englishman);
+		variables.add(Spaniard);
+		variables.add(Norwegian);
+		variables.add(Ukrainian);
+		variables.add(Japanese);
+
+		variables.add(OldGold);
+		variables.add(Kools);
+		variables.add(Chesterfields);
+		variables.add(LuckyStrike);
+		variables.add(Parliaments);
+
+		variables.add(Water);
+		variables.add(OrangeJuice);
+		variables.add(Tea);
+		variables.add(Coffee);
+		variables.add(Milk);
+
+		variables.add(Zebra);
+		variables.add(Dog);
+		variables.add(Fox);
+		variables.add(Snails);
+		variables.add(Horse);
+
 		
 		csp = new CSP(variables);
 
 		// TODO set domains of variables, e.g.,
-		// Domain d1 = new Domain(new String[]{"foo", "bar"});
-		// csp.setDomain(var1, d1);
-		// Domain d2 = new Domain(new Integer[]{1, 2});
-		// csp.setDomain(var2, d2);
+		Domain d = new Domain(houseNumbers);
+//		# The Englishman lives in the red house.
+//		# The Spaniard owns the dog.
+//		# Coffee is drunk in the green house.
+//		# The Ukrainian drinks tea.
+//		# The green house is immediately to the right of the ivory house.
+//		# The Old Gold smoker owns snails.
+//		# Kools are smoked in the yellow house.
+//		# Milk is drunk in the middle house.
+//		# The Norwegian lives in the first house.
+//		# The man who smokes Chesterfields lives in the house next to the man with the fox.
+//		# Kools are smoked in the house next to the house where the horse is kept.
+//		# The Lucky Strike smoker drinks orange juice.
+//		# The Japanese smokes Parliaments.
+//		# The Norwegian lives next to the blue house.
+
+		for(Variable v: variables)
+		{
+			if(v.getName().equals("Norwegian"))
+			{
+				csp.setDomain(v, new Domain(new String[] {"1"} ));
+			}
+			else if(v.getName().equals("Milk"))
+			{
+				csp.setDomain(v, new Domain(new String[] {"3"} ));
+			}
+			else
+			{
+				csp.setDomain(v, d);
+			}
+		}
+
 		
 		// TODO add constraints, e.g.,
-		// csp.addConstraint(new NotEqualConstraint(var1, var2)); // meaning var1 != var2
-		// csp.addConstraint(new EqualConstraint(var1, var2)); // meaning var1 == var2
-		// csp.addConstraint(new SuccessorConstraint(var1, var2)); // meaning var1 == var2 + 1
-		// csp.addConstraint(new DifferByOneConstraint(var1, var2)); // meaning var1 == var2 + 1 or var1 == var2 - 1 
+		csp.addConstraint(new EqualConstraint(Englishman, Red));
+		//csp.addConstraint(new NotEqualConstraint(var1, var2)); // meaning var1 != var2
+		//csp.addConstraint(new EqualConstraint(var1, var2)); // meaning var1 == var2
+		//csp.addConstraint(new SuccessorConstraint(var1, var2)); // meaning var1 == var2 + 1
+		//csp.addConstraint(new DifferByOneConstraint(var1, var2)); // meaning var1 == var2 + 1 or var1 == var2 - 1
 		
 		return csp;
 	}
